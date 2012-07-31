efc6446ee936686992dc04b4dfd18b55efa50953
cron <cron@debcut.vb>
2012-07-31 23:58:34 +0300

    implemented basic proxying using config

--- src/magic.go
+++ src/magic.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-    /*"errors"*/
+	"errors"
+	"path"
 )
 
 func match(pat string, addrs map[string]interface{}) (string, error) {
-    return "/", nil//errors.New("no match found")
+	res := ""
+	for k, _ := range addrs {
+		n := len(k)
+		if n == 0 || (k[n-1] != '/' && k != pat) || len(pat) < n || pat[0:n] != k {
+			continue
+		}
+		if len(k) > len(res) {
+			res = k
+		}
+	}
+	if res == "" {
+		return res, errors.New("no match found")
+	}
+	return res, nil
+}
+
+func cleanPath(p string) string {
+	if p == "" {
+		return "/"
+	}
+	if p[0] != '/' {
+		p = "/" + p
+	}
+	np := path.Clean(p)
+	if p[len(p)-1] == '/' && np != "/" {
+		np += "/"
+	}
+	return np
 }
--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	conf "github.com/AndriyLytvynov/goconf"
 	"log"
@@ -17,9 +18,9 @@ const (
 
 func main() {
 	log.Println("bifrost started.")
-    if err := conf.LoadConfig(conf_path); err != nil {
-        log.Fatal("could not read configuration file :", err)
-    }
+	if err := conf.LoadConfig(conf_path); err != nil {
+		log.Fatal("could not read configuration file :", err)
+	}
 	addr, err := conf.Get("addr")
 	if err != nil {
 		log.Println("addr parameter not specified, using default -", def_addr)
@@ -30,43 +31,72 @@ func main() {
 		log.Println("invalid addr parameter specified, using default -", def_addr)
 		address = def_addr
 	}
-    http.HandleFunc("/", proxyHandler)
+	http.HandleFunc("/", proxyHandler)
 	if err = http.ListenAndServe(address, nil); err != nil {
 		log.Fatalln(err)
-		log.Println("bifrost stopped.")
 	}
 }
 
 func proxyHandler(resp http.ResponseWriter, req *http.Request) {
-    defer func(){
-        if err := recover(); err != nil {
-            log.Println("error :", err)
-        }
-    }()
-    log.Println("request to ", req.RequestURI)
-    tmp, err := conf.Get("proxy_info")
-    if err != nil {
-        log.Println("failed to read config :", err)
-        http.NotFound(resp, req)
-        return
-    }
-    addrs, ok := tmp.(map[string]interface{})
-    if !ok {
-        log.Println("config is not formatted well!")
-        http.NotFound(resp, req)
-        return
-    }
-    route, err := match(req.URL.Path, addrs)
-    if err != nil {
-        log.Println("could not find route for request :", req.URL.Path)
-        http.NotFound(resp, req)
-        return
-    }
-    dest, err := url.Parse(addrs[route].([]string)[0])
-    if err != nil {
-        log.Println("config url not valid :", addrs[route].([]string)[0])
-        http.NotFound(resp, req)
-        return
-    }
-    httputil.NewSingleHostReverseProxy(dest).ServeHTTP(resp, req)
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("error :", err)
+			http.Error(resp, err.(error).Error(), http.StatusInternalServerError)
+		}
+	}()
+	log.Println("request to ", req.Host+req.URL.Path)
+	if p := cleanPath(req.URL.Path); p != req.URL.Path {
+		log.Println("cleaned up request url", req.URL.Path, "->", p)
+		resp.Header().Set("Location", p)
+		resp.WriteHeader(http.StatusMovedPermanently)
+		return
+	}
+	addrs, err := getConf()
+	if err != nil {
+		log.Println("failed to read config :", err)
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	dest, err := findRoute(req, addrs)
+	if err != nil {
+		log.Println("could not find route for request :", err)
+		http.NotFound(resp, req)
+		return
+	}
+	httputil.NewSingleHostReverseProxy(dest).ServeHTTP(resp, req)
+}
+
+func getConf() (map[string]interface{}, error) {
+	tmp, err := conf.Get("proxy_info")
+	if err != nil {
+		return nil, err
+	}
+	addrs, ok := tmp.(map[string]interface{})
+	if !ok {
+		return nil, errors.New("bad configuration : proxy_info should be map[string][]string")
+	}
+	return addrs, nil
+}
+
+func findRoute(req *http.Request, addrs map[string]interface{}) (*url.URL, error) {
+	route, err := match(req.URL.Path, addrs)
+	if err != nil || addrs[route] == nil {
+		route, err = match(req.Host+req.URL.Path, addrs)
+		if err != nil || addrs[route] == nil {
+			return nil, err
+		}
+	}
+	routes, ok := addrs[route].([]interface{})
+	if !ok {
+		return nil, errors.New("bad configuration : " + route + " should be []string")
+	}
+	dest, ok := routes[0].(string) // TODO balancing
+	if !ok {
+		return nil, errors.New("bad configuration : " + route + " should bs []string")
+	}
+	res, err := url.Parse(dest)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
